47bf8b81cae8003281409ed85cccf538f3df6370
Andrea Corna <andrea.corna.ac.91@gmail.com>
2013-07-12 20:30:25 +0200

    risolto problema host

--- host.h
+++ host.h
@@ -7,14 +7,15 @@ class host
 public:
     host(QString ip);
     QString get_ip();
-    void set_mtu_packet(mtu_packet* packet);
-    void set_syn_packet(syn_packet* packet);
-    void set_http_packet(http_packet* packet);
-    void set_uptime_packet(uptime_packet* packet);
+    void set_mtu_packet(mtu_info* packet);
+    void set_syn_packet(syn_info* packet);
+    void set_http_packet(http_info* packet);
+    void set_uptime_packet(uptime_info* packet);
     mtu_info* get_mtu_packet();
     syn_info* get_syn_packet();
-    http_info* get_http_packet();
     uptime_info* get_uptime_packet();
+    http_info* get_http_packet();
+
 private:
     QString ip_host;
     mtu_info* mtu_packet;
