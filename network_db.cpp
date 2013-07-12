34e57c0e50976d97a60384563d0ae90a8cf4b9da
Lorenzo Fontana <lord.fontana@gmail.com>
2013-07-12 18:10:58 +0200

    commit per forza

--- network_db.cpp
+++ network_db.cpp
@@ -1,5 +1,17 @@
 #include "network_db.h"
 
 network_db::network_db()
-{
+{   
 }
+host* network_db::find_host(QString addr){
+    for(int i=0;i<network.size();i++){
+        if (addr.compare(network[i].get_ip()))
+            return network[i];
+    }
+    return NULL;
+}
+
+void network_db::add_packet(p0f_info packet){
+
+}
+
