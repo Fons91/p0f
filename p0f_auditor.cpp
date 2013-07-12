cf624e4bf34d43f97fae5e3a7312096ebb45402f
Andrea Corna <andrea.corna.ac.91@gmail.com>
2013-07-12 15:09:49 +0200

    modifica create packet

--- p0f_auditor.cpp
+++ p0f_auditor.cpp
@@ -3,6 +3,8 @@
 #include <stdio.h>
 #include "p0f_info.h"
 
+
+
 p0f_info* packet;
 
 void create_packet(char* client, int client_port, char* server, int server_port, char *keyword){
@@ -11,8 +13,21 @@ void create_packet(char* client, int client_port, char* server, int server_port,
     qDebug()<<QString(server);
     qDebug()<<server_port;
     qDebug()<<keyword;
+    p0f_info_factory::info_type tmp;
+    if(strcmp(keyword,"mtu")){
+        tmp = p0f_info_factory::MTU;
+    }
+    if(strcmp(keyword,"syn")){
+        tmp = p0f_info_factory::SYN;
+    }
+    if(strcmp(keyword,"http_response")){
+        tmp = p0f_info_factory::HTTP_RESPONSE;
+    }
+    if(strcmp(keyword,"uptime")){
+        tmp = p0f_info_factory::UPTIME;
+    }
 
-    //packet = p0f_info_factory::new_p0f_info();
+    //packet = p0f_info_factory::new_p0f_info(tmp,client);
 
 }
 
